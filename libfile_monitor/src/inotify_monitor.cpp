62d1f0545683ab417480a0df2741d81c79748ac0
hao.chen <hao.chen@qingteng.cn>
2019-11-27 22:30:51 +0800

    add inotify monitor implemention

--- libfile_monitor/src/inotify_monitor.cpp
+++ libfile_monitor/src/inotify_monitor.cpp
@@ -1,1 +1,41 @@
+#include <sstream>
+#include <ctime>
+#include <cmath>
+#include <set>
+#include <map>
+#include <sys/select.h>
+#include <limits.h>
+#include "exception.h"
+#include "path_utils.h"
+#include "monitor.h"
 #include "inotify_monitor.h"
+
+using namespace std;
+
+namespace fm {
+
+    struct inotify_monitor_impl {
+        int inotify_monitor_handle = -1;
+        std::vector<event> events;
+
+        set<int> watched_descriptors;
+        map<string ,int> path_to_wd;
+
+        map<int, string> wd_to_path;
+        set<int> descriptors_to_remove;
+        set<int> watches_to_remove;
+        vector<string> paths_to_rescan;
+        time_t curr_time;
+    };
+
+    static const unsigned int BUFFER_SIZE = (10 * 10 * ((sizeof(struct inotify_event)) + NAME_MAX + 1));
+
+    Inotify_monitor::Inotify_monitor(std::vector <string> paths,
+                                     FM_EVENT_CALLBACK *callback,
+                                     void *context) :
+       Monitor(paths, callback, context),
+       impl(new inotify_monitor_impl())
+                                     {
+
+    }
+}
--- libfile_monitor/src/inotify_monitor.h
+++ libfile_monitor/src/inotify_monitor.h
@@ -2,4 +2,42 @@
 #ifndef FILE_MONITOR_INOTIFY_MONITOR_H
 #define FILE_MONITOR_INOTIFY_MONITOR_H
 
+#include <sys/inotify.h>
+#include <string>
+#include <vector>
+#include <sys/stat.h>
+#include "monitor.h"
+
+namespace fm {
+    struct inotify_monitor_impl;
+
+    class Inotify_monitor : public Monitor {
+    public:
+        Inotify_monitor(std::vector<std::string> paths,
+                        FM_EVENT_CALLBACK *callback,
+                        void *context);
+        virtual ~Inotify_monitor();
+
+    protected:
+        void run();
+
+    private:
+        Inotify_monitor(const Inotify_monitor &orig) = delete;
+        Inotify_monitor &operator=(const Inotify_monitor &that) delete;
+
+        void scan_root_paths();
+        bool is_watched(const std::string &path) const;
+        void preprocess_dir_event(struct inotify_event *event);
+        void preprocess_event(struct inotify_event *event);
+        void preprocess_node_event(struct inotify_event *event);
+        void scan(const std::string &path, const bool accept_non_dirs = true);
+        bool add_watch(const std::string &path,
+                      const struct stat &fd_stat);
+        void process_pending_events();
+        void remove_watch(int fd);
+
+        inotiry_monitor_impl *impl;
+    };
+}
+
 #endif //FILE_MONITOR_INOTIFY_MONITOR_H
