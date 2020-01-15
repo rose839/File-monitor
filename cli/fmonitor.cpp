2b58850943bc3c9ce17472194f46c5f059c6d05b
rose839 <617418745@qq.com>
2020-01-15 23:24:50 +0800

    cli

--- cli/fmonitor.cpp
+++ cli/fmonitor.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string>
 #include "fmonitor.h"
 #include "config.h"
 
-usint std::ostream;
+using std::ostream;
+using std::string;
+using namespace fm;
+
+/*
+ * OPT_* variables are used as getopt_long values for long options that do not
+ * have a short option equivalent.
+ */
+static const int OPT_BATCH_MARKER = 128;
+static const int OPT_FORMAT = 129;
+static const int OPT_EVENT_FLAG_SEPARATOR = 130;
+static const int OPT_EVENT_TYPE = 131;
+static const int OPT_ALLOW_OVERFLOW = 132;
+static const int OPT_MONITOR_PROPERTY = 133;
+static const int OPT_FIRE_IDLE_EVENTS = 134;
+static const int OPT_FILTER_FROM = 135;
 
 static void usage(ostream &stream) {
 	stream << "\n\n";
@@ -47,6 +63,43 @@ static void usage(ostream &stream) {
 }
 
 static void parse_opts(int argc, char **argv) {
+	int ch;
+  	string short_options = "01ade:Ef:hi:Il:LMm:nortuvx";
+	int option_index = 0;
+	static struct option long_options[] = {
+	{"access",               no_argument,       nullptr,       'a'},
+	{"allow-overflow",       no_argument,       nullptr,       OPT_ALLOW_OVERFLOW},
+	{"batch-marker",         optional_argument, nullptr,       OPT_BATCH_MARKER},
+	{"directories",          no_argument,       nullptr,       'd'},
+	{"event",                required_argument, nullptr,       OPT_EVENT_TYPE},
+	{"event-flags",          no_argument,       nullptr,       'x'},
+	{"event-flag-separator", required_argument, nullptr,       OPT_EVENT_FLAG_SEPARATOR},
+	{"exclude",              required_argument, nullptr,       'e'},
+	{"extended",             no_argument,       nullptr,       'E'},
+	{"filter-from",          required_argument, nullptr,       OPT_FILTER_FROM},
+	{"fire-idle-events",     no_argument,       nullptr,       OPT_FIRE_IDLE_EVENTS},
+	{"follow-links",         no_argument,       nullptr,       'L'},
+	{"format",               required_argument, nullptr,       OPT_FORMAT},
+	{"format-time",          required_argument, nullptr,       'f'},
+	{"help",                 no_argument,       nullptr,       'h'},
+	{"include",              required_argument, nullptr,       'i'},
+	{"insensitive",          no_argument,       nullptr,       'I'},
+	{"latency",              required_argument, nullptr,       'l'},
+	{"list-monitors",        no_argument,       nullptr,       'M'},
+	{"monitor",              required_argument, nullptr,       'm'},
+	{"monitor-property",     required_argument, nullptr,       OPT_MONITOR_PROPERTY},
+	{"numeric",              no_argument,       nullptr,       'n'},
+	{"one-per-batch",        no_argument,       nullptr,       'o'},
+	{"one-event",            no_argument,       nullptr,       '1'},
+	{"print0",               no_argument,       nullptr,       '0'},
+	{"recursive",            no_argument,       nullptr,       'r'},
+	{"timestamp",            no_argument,       nullptr,       't'},
+	{"utc-time",             no_argument,       nullptr,       'u'},
+	{"verbose",              no_argument,       nullptr,       'v'},
+	{"version",              no_argument,       &version_flag, true},
+	{nullptr, 0,                                nullptr,       0}
+	};
+
 	
 }
 
